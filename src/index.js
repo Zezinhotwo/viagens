a89c10f5294a43ef6bcfdf3fbce868b647f492a2
Zezinhodois <123456789+kaio.bg12@gmail.com>
2024-07-15 19:57:46 -0300

    duas sections(destinos e reservas)

--- src/index.js
+++ src/index.js
@@ -11,4 +11,11 @@ DomImgMain.src = Traveller;
 import ImgItem from "./assets/img/satellite.png";
 const DomImgItem = document.querySelectorAll(".image-item1");
 // DomImgItem.src = ImgItem;
-DomImgItem.forEach(img=> img.src=ImgItem)
+DomImgItem.forEach((img) => (img.src = ImgItem));
+import ImgDestination from "./assets/img/Destination.png";
+const DomDestination = document.querySelectorAll(".destination");
+DomDestination.forEach((img) => (img.src = ImgDestination));
+
+import ImgIconeOne from "./assets/img/iconOne.png";
+const DomIcone = document.querySelectorAll(".icone");
+DomIcone.forEach((img) => (img.src = ImgIconeOne));
